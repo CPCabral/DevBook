a8401fec8ac927bc9051e1b6bde22ffba436ddfd
CPCabral <camiladeepaula@gmail.com>
2025-06-30 16:25:18 -0300

    pacote middlewares

--- main.go
+++ main.go
@@ -22,8 +22,6 @@ func main() {
 	config.Carregar()
 	r := router.Gerar()
 
-	fmt.Println(config.SecretKey)
-
 	fmt.Printf("Escutando na porta %d\n", config.Porta)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d",config.Porta), r))
 }
