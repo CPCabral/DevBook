6f54c973e55e4c38580581c734f4595c5fd820f1
CPCabral <camiladeepaula@gmail.com>
2025-07-07 09:48:55 -0300

    validação e formatação de publicação

--- src/modelos/Publicacao.go
+++ src/modelos/Publicacao.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Publicação representa uma publicação feita por um usuário
 type Publicacao struct {
@@ -11,4 +15,28 @@ type Publicacao struct {
 	AutorNick string `json:"autor_nick,omitempty"`
 	Curtidas  uint64 `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadaEm"`
+}
+
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.Validar(); erro != nil {
+		return erro
+	}
+	publicacao.formatar()
+	return nil
+}
+
+func (publicacao *Publicacao) Validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("o título da publicação não pode estar vazio")
+	}
+	if publicacao.Conteudo == "" {
+		return errors.New("o conteúdo da publicação não pode estar vazio")
+	}
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
+	publicacao.CriadaEm = time.Now()	
 }
