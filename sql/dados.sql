9ddb65a0b15d8f8f5e5d119a3da586bdfa14643a
CPCabral <camiladeepaula@gmail.com>
2025-07-09 16:57:20 -0300

    busca de publicações

--- sql/dados.sql
+++ sql/dados.sql
@@ -10,3 +10,9 @@ insert into seguidores (usuario_id, seguidor_id) values
 (2, 3),
 (3, 1),
 (3, 2);
+
+insert into publicacoes (titulo, conteudo, autor_id)
+values
+("Publicação do usuario 1", "Essa é a publicação do usuario 1", 1),
+("Publicação do usuario 2", "Essa é a publicação do usuario 2", 2),
+("Publicação do usuario 3", "Essa é a publicação do usuario 3", 3);
--- src/controllers/publicacoes.go
+++ src/controllers/publicacoes.go
@@ -61,7 +61,27 @@ func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
 
 // BuscarPublicacoes busca todas as publicações que aparece no feed do usuario
 func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
 
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return		
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacoes, erro := repositorio.Buscar(usuarioID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, publicacoes)
 }
 
 // BuscarPublicacao busca uma publicação específica por ID
