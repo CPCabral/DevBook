e8242a4cee97277a731d1a74467177798e48420b
CPCabral <camiladeepaula@gmail.com>
2025-07-16 09:23:04 -0300

    Middlewares e autenticação

---
+++ webapp/src/middlewares/middlewares.go
@@ -0,0 +1,27 @@
+package middlewares
+
+import (
+	"log"
+	"net/http"
+	"webapp/src/cookies"
+)
+
+// Logger escreve no terminal os logs de cada requisição recebida
+func Logger(proximaFuncao http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
+		proximaFuncao(w, r)
+	}
+}
+
+// Autenticar verifica a existencia de cookies
+func Autenticar(proximaFuncao http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if _, erro := cookies.Ler(r); erro != nil {
+			http.Redirect(w, r, "/login", 302)
+			return
+		}
+		proximaFuncao(w, r)
+	}
+}
+//func proximaFuncao(w http.ResponseWriter, r *http.Request){}
