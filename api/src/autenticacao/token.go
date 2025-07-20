9572666abdc388ea3ca7d3cf8e0b0799af9e0f27
CPCabral <camiladeepaula@gmail.com>
2025-07-20 15:48:12 -0300

    alteração tempo de expiração do token

--- api/src/autenticacao/token.go
+++ api/src/autenticacao/token.go
@@ -15,7 +15,7 @@ import (
 func CriarToken(usuarioID uint64) (string, error){
 	permissoes := jwt.MapClaims{}
 	permissoes["authorized"] = true
-	permissoes["exp"] = time.Now().Add(time.Minute * 6).Unix()
+	permissoes["exp"] = time.Now().Add(time.Hour * 6).Unix()
 	permissoes["usuarioId"] = usuarioID
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissoes)
 	return token.SignedString([]byte(config.SecretKey)) //secret
